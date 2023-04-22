5fa459d7bf2d29eb144d31af5801372d72e323c5
wdd130wei <zhiwade.wei@gmail.com>
2023-04-22 16:17:53 +1200

    update 2204

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your score: ");
+        int score = int.Parse(Console.ReadLine());
+
+        string passMessage = "You passed the exam.";
+
+        string failedMessage = "Study harder next time.";
+
+        if (score >= 70)
+        {
+            
+
+            if (score >= 90 && score <= 100)
+            {
+                Console.WriteLine($" {passMessage} Your grade is A.");
+            }
+            
+            else if (score >= 80 && score < 90)
+            {
+                Console.WriteLine($"{passMessage} Your grade is B.");
+            }
+            
+            else if (score >= 70 && score <80)
+            {
+                Console.WriteLine($"{passMessage} Your grade is C.");
+            }
+        }
+
+        else
+        {
+;
+
+            if (score >= 60 && score < 70)
+            {
+                Console.WriteLine($" {failedMessage} Your grade is D.");
+            }
+
+            else if (score >= 0 && score < 60)
+            {
+                Console.WriteLine($" {failedMessage} Your grade is F.");
+            }
+        }
+
     }
 }
