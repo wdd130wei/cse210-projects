878013d2998c2563c3099583ec0a0826e1170220
wdd130wei <zhiwade.wei@gmail.com>
2023-04-30 21:48:04 +1200

    update 30/04

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction number1 = new Fraction();
+        Fraction number2 = new Fraction(6);
+        Fraction number3 = new Fraction(6,7);
+        Console.WriteLine(number1.GetFractionString());
+        Console.WriteLine(number2.GetFractionString());
+        Console.WriteLine(number3.GetFractionString());
+
+        Fraction number4 = new Fraction();
+        number4.SetTop(7);
+        Console.WriteLine(number4.GetTop());
+        Console.WriteLine(number4.GetFractionString());
+
+        Console.WriteLine(number3.GetDecimalValue());
     }
 }
