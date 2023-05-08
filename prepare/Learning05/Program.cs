6aea2d86b165bc88e3d9d74a47af69da18368eba
wdd130wei <zhiwade.wei@gmail.com>
2023-05-08 18:22:33 +1200

    update 08.05.23

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Blue", 5));
+        shapes.Add(new Circle("Red", 5));
+        shapes.Add(new Rectangle("Yellow", 5, 7));
+
+        foreach (Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+            string color = shape.GetColor();
+            Console.WriteLine ($"{area} {color}");
+        }
     }
 }
